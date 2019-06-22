15a525c4dcad5deb55a9d0025d1cad4ad900eef1
Frixs <Frixs@users.noreply.github.com>
2019-06-22 23:49:14 +0200

    Overlay window.

--- Production/BlackSpiritTimerApp/Windows/OverlayWindow.xaml.cs
+++ Production/BlackSpiritTimerApp/Windows/OverlayWindow.xaml.cs
@@ -6,7 +6,6 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Forms;
 
-
 namespace BlackSpiritTimerApp.Windows
 {
     /// <summary>
@@ -15,20 +14,19 @@ namespace BlackSpiritTimerApp.Windows
     public partial class OverlayWindow : Window
     {
         /** Target window name. */
-        //public const string WINDOW_NAME = "Black Desert - 327975";
-        public const string WINDOW_NAME = "Calculator";
-        /** Target window handle. */
-        private IntPtr targetWindowHandle;
+        private const string WindowName = "Calculator"; // TODO.
+
+        private bool isOverlayOk = true;
 
         //[DllImport("user32.dll")]
         //private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
         //[DllImport("user32.dll", SetLastError = true)]
         //private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        //[DllImport("user32.dll")]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //private static extern bool GetWindowRect(IntPtr hwd, out WindowCornerPosition lpRect);
         [DllImport("user32.dll", SetLastError = true)]
         private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool GetWindowRect(IntPtr hwd, out WindowCornerPosition lpRect);
 
         public OverlayWindow()
         {
@@ -38,14 +36,17 @@ namespace BlackSpiritTimerApp.Windows
         private void Window_Initialized(object sender, EventArgs e)
         {
             IntPtr overlayWindowHandle = new WindowInteropHelper(this).Handle;
-            targetWindowHandle = FindWindow(null, WINDOW_NAME);
+            IntPtr targetWindowHandle;
+
+        targetWindowHandle = FindWindow(null, WindowName);
             if (targetWindowHandle.Equals(IntPtr.Zero))
             {
-                Console.WriteLine("Target window not found!");
-                // TODO: make variable to dont show bad window, in Load method.
+                Console.WriteLine("Target window not found!"); // TODO - logger.
+                isOverlayOk = false;
+                return;
             }
 
-            this.Background = new SolidColorBrush(Colors.LightGray); // For DEBUG.
+            //this.Background = new SolidColorBrush(Colors.LightGray); // For DEBUG.
             this.ResizeMode = ResizeMode.NoResize;
             this.Topmost = true;
 
@@ -53,31 +54,46 @@ namespace BlackSpiritTimerApp.Windows
             //int initialStyle = GetWindowLong(overlayWindowHandle, -20);
             //SetWindowLong(overlayWindowHandle, -20, initialStyle | 0x80000 | 0x20);
 
-            // Create on the position of target window.
-            WindowCornerPosition rectWindow;
-            GetWindowRect(targetWindowHandle, out rectWindow);
-
-            Screen[] screens = Screen.AllScreens;
-            Screen screen = Screen.FromHandle(targetWindowHandle); //this is the Form class
-            Console.WriteLine(screen);
-            // TODO: Solve screen position & DPI.
-
-            this.Width = rectWindow.right - rectWindow.left;
-            this.Height = rectWindow.bottom - rectWindow.top;
-            this.Top = rectWindow.top;
-            this.Left = 0;// rectWindow.left;
+            SetOverlayPosition(overlayWindowHandle, targetWindowHandle);
         }
 
-        // Inner struct to hold data about target window corner positions.
-        public struct WindowCornerPosition
+        private void DockPanel_Loaded(object sender, RoutedEventArgs e)
         {
-            public int left, top, right, bottom;
+            if (!isOverlayOk)
+            {
+                this.Close();
+                return;
+            }
+
+            this.WindowState = WindowState.Maximized;
         }
 
-        enum DisplayAffinity : uint
+        /**
+         * Create overlay window on the position of target window.
+         */
+        private void SetOverlayPosition(IntPtr overlayWindowHandle, IntPtr targetWindowHandle)
         {
-            None = 0,
-            Monitor = 1
+            //WindowCornerPosition rectWindow;
+            Screen screen = null;
+
+            //GetWindowRect(targetWindowHandle, out rectWindow);
+
+            screen = Screen.FromHandle(targetWindowHandle);
+
+            this.WindowState = WindowState.Normal; // Reset.
+
+            this.Left = screen.WorkingArea.Left;
+            this.Top = screen.WorkingArea.Top;
+            this.Width = screen.WorkingArea.Width;
+            this.Height = screen.WorkingArea.Height;
         }
+
+        /** 
+         * Inner struct to hold data about target window corner positions. 
+         */
+        //public struct WindowCornerPosition
+        //{
+        //    public int left, top, right, bottom;
+        //}
     }
 }
