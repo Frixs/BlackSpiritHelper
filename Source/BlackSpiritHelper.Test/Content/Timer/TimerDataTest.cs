8d571266f2b5ae3aafe515371dcc9c03d95addbc
Frixs <17912365+Frixs@users.noreply.github.com>
2019-10-18 23:21:14 -0500

    UnitTest possible solution preparation

---
+++ Source/BlackSpiritHelper.Test/Content/Timer/TimerDataTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackSpiritHelper.Test
+{
+    [TestClass]
+    public class TimerDataTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
