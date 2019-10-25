1ac5f6dd40ff3a0897eb4da607025a617e30b368
Frixs <17912365+Frixs@users.noreply.github.com>
2019-10-24 23:49:23 -0500

    Unit test structure set

--- Source/BlackSpiritHelper.Test/Content/Timer/TimerDataTest.cs
+++ Source/BlackSpiritHelper.Test/Content/Timer/TimerDataTest.cs
@@ -3,21 +3,28 @@
 namespace BlackSpiritHelper.Test
 {
     [TestClass]
-    public class TimerDataTest
+    public class TimerDataTest : BaseTest
     {
         [TestInitialize]
         public void TestInitialize()
         {
+            TestInitializeMethod();
         }
 
         [TestCleanup]
         public void TestClean()
         {
+            TestCleanMethod();
         }
 
+        /// <summary>
+        /// TODO: Unit tests
+        ///     : App.xaml.cs should have separate class to set initialization of IoC. We cannot access App class atm.
+        /// </summary>
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.IsTrue(true);
         }
     }
 }
