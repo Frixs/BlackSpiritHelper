d86d6a27a90055ed5b62b0b10c4a5d27d735ce50
Frixs <17912365+Frixs@users.noreply.github.com>
2019-11-04 11:14:01 -0600

    Watchdog data & user settings init

---
+++ Source/BlackSpiritHelper.Core/ViewModels/Content/Watchdog/Data/WatchdogDataViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace BlackSpiritHelper.Core
+{
+    public class WatchdogDataViewModel : DataContentBaseViewModel<WatchdogDataViewModel>
+    {
+        #region Public Properties
+
+        [XmlIgnore]
+        public override bool IsRunning 
+        {
+            get => false;
+            protected set => throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public WatchdogDataViewModel()
+        {
+        }
+
+        protected override void SetDefaultsMethod()
+        {
+        }
+
+        protected override void SetupMethod()
+        {
+        }
+
+        protected override void UnsetMethod()
+        {
+        }
+
+        #endregion
+    }
+}
