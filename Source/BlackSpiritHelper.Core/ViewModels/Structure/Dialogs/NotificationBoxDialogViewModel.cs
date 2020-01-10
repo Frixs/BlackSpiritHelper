103244a12ef8fa0bc6fa4d2531c9e74d9507fca0
Frixs <17912365+Frixs@users.noreply.github.com>
2020-01-10 19:45:55 +0100

    Notification Button Templates

--- Source/BlackSpiritHelper.Core/ViewModels/Structure/Dialogs/NotificationBoxDialogViewModel.cs
+++ Source/BlackSpiritHelper.Core/ViewModels/Structure/Dialogs/NotificationBoxDialogViewModel.cs
@@ -1,5 +1,13 @@
-﻿namespace BlackSpiritHelper.Core
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BlackSpiritHelper.Core
 {
+    /// <summary>
+    /// Represents notification context of notification UI.
+    /// List of all notifications and calling notifications can be found in <see cref="IoC.UI"/>
+    /// </summary>
     public class NotificationBoxDialogViewModel
     {
         #region Public Properties
@@ -14,6 +22,30 @@
         /// </summary>
         public string Message { get; set; } = "Message placeholder!";
 
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public NotificationBoxResult Result { get; set; } = NotificationBoxResult.Ok;
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Command to run OK event.
+        /// </summary>
+        public ICommand OkCommand { get; set; }
+
+        /// <summary>
+        /// Command to run OK event.
+        /// </summary>
+        public ICommand YesCommand { get; set; }
+
+        /// <summary>
+        /// Command to run OK event.
+        /// </summary>
+        public ICommand NoCommand { get; set; }
+
         #endregion
 
         #region Constructor
@@ -23,6 +55,27 @@
         /// </summary>
         public NotificationBoxDialogViewModel()
         {
+            // Create commands.
+            CreateCommands();
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// Create commands.
+        /// </summary>
+        private void CreateCommands()
+        {
+            OkCommand = new RelayCommand(async () => await OkCommandMethodAsync());
+            YesCommand = new RelayCommand(async () => await OkCommandMethodAsync());
+            NoCommand = new RelayCommand(async () => await OkCommandMethodAsync());
+        }
+
+        private Task OkCommandMethodAsync()
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
